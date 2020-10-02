9ae3a7fde4a370f42d28caa18888ee4738063575
James Lamb <jameslamb13@gmail.com>
2020-10-02 15:13:13 -0400

    fixed issue with delete function. fully working.

--- server.js
+++ server.js
@@ -1,51 +1,61 @@
 var express = require("express");
 const fs = require("fs");
 var path = require("path");
-const db = require("./db/db.json")
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
-   
+app.use(express.json());
+
 // API Routes
 
-
-app.get("/api/notes/", function(req, res){
-    fs.readFile("./db/db.json", "utf8", function(err){
-    if (err)
-    return err
+app.get("/api/notes/", function (req, res) {
+    fs.readFile("./db/db.json", "utf8", function (err, db) {
+        if (err) {
+            console.log(err)
+            return err
+        }
+        console.log(db)
+        return res.json(JSON.parse(db))
     })
-    return res.json(db)
-   
+
+
 });
 
 
-app.post("/api/notes/", function(req, res){
-    
-    var newNote = req.body;
-    newNote.id = db.length;
-    console.log(newNote);
-    db.push(newNote);
-    
+app.post("/api/notes/", function (req, res) {
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
+        var newNote = req.body;
+        var db = JSON.parse(data)
+        newNote.id = db.length;
+        console.log(newNote);
+        db.push(newNote);
 
-    
-    fs.writeFileSync("./db/db.json", JSON.stringify(db), "utf8", function(err){
-        if (err)
-        return err
+        fs.writeFile("./db/db.json", JSON.stringify(db), "utf8", function (err) {
+            if (err) {
+                console.log(err)
+                return err
+            }
+            return res.json(newNote)
+        })
     })
-    return res.end();
 });
 
-app.delete("/api/notes/:id", function(req, res){
-
-const newDb = db.filter(note => note.id != req.params.id);
-    fs.writeFileSync("./db/db.json", JSON.stringify(newDb), "utf8", function(err){
-        if (err)
-        return err
+app.delete("/api/notes/:id", function (req, res) {
+    console.log(req.params.id)
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
+        var db = JSON.parse(data)
+        const newDb = db.filter(note => note.id != req.params.id);
+        fs.writeFile("./db/db.json", JSON.stringify(newDb), "utf8", function (err) {
+            if (err) {
+                console.log(err)
+                return err
+            }
+            console.log("test")
+            return res.json(newDb);
+        });
     });
-      return res.json(newDb);
 });
 
 app.use(express.static('public'));
@@ -55,15 +65,15 @@ app.use(express.static('public'));
 
 // HTML ROUTES
 
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log(`App listening on PORT http://localhost:${PORT}/`);
 });
